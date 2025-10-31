f67ebffff4d2e003dd89484f4e89adfa7733c7ab
lucianodacosta-12343 <luciano.dacostabraga@chevron.com>
2025-10-31 12:38:39 -0300

    Change logger level to WARNING for stream handler

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -12,10 +12,9 @@ app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
 
-app.logger.setLevel(logging.INFO)
-handler = logging.StreamHandler()
-handler.setLevel(logging.INFO)
-app.logger.addHandler(handler)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.WARNING)
+app.logger.addHandler(streamHandler)
 
 
 Session(app)
