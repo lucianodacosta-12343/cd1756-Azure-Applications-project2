a54e6a4b459e254d72bfaa0e6bfc4ab0bb32b825
lucianodacosta-12343 <luciano.dacostabraga@chevron.com>
2025-10-31 12:11:58 -0300

    Fix logging messages for login attempts

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -68,14 +68,15 @@ def login():
 
 
         if user is None or not user.check_password(form.password.data):
-            app.logger.warning(f"Login successful for user: {form.username.data}")
+            
+            app.logger.warning(f"Failed login attempt for user: {form.username.data}")
             flash('Invalid username or password')
 
             
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
 
-        app.logger.info(f"Failed login attempt for user: {user.username}")
+        app.logger.info(f"Login successful for user: {form.username.data}")
         
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
